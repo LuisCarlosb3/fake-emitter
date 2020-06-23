bb86f2a2bf8d0775841c1a88a99d45e8dd682678
Luis Carlos <lu.ca.oliveira41@gmail.com>
2020-06-23 11:14:46 -0400

    chore: initial commit with project structure

---
+++ index.js
@@ -0,0 +1,57 @@
+const { app, BrowserWindow, Menu } = require('electron')
+let win
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 500,
+    height: 730,
+    title: 'FakeEmitter',
+    titleBarStyle: 'hidden',
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+  // and load the index.html of the app.
+  win.loadFile('src/views/index.html')
+  // false to resize
+  win.setResizable(false)
+  // Build menu from template
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate)
+  // Insert menu
+  Menu.setApplicationMenu(mainMenu)
+  win.on('closed', function () {
+    app.quit()
+  })
+}
+const mainMenuTemplate = [
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'Quit',
+        accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+        click () {
+          app.quit()
+        }
+      }
+    ]
+  }
+]
+if (process.env.NODE_ENV !== 'production') {
+  mainMenuTemplate.push({
+    label: 'Developer Tools',
+    submenu: [
+      {
+        role: 'reload'
+      },
+      {
+        label: 'Toggle DevTools',
+        accelerator: process.platform === 'darwin' ? 'Command+I' : 'Ctrl+I',
+        click (item, focusedWindow) {
+          focusedWindow.toggleDevTools()
+        }
+      }
+    ]
+  })
+}
+app.whenReady().then(createWindow)
