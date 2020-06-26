476369e6cc534e6f6263904405e5082dbcbde6f6
Luis Carlos <lu.ca.oliveira41@gmail.com>
2020-06-26 10:02:47 -0400

    feat: create equipament modal

--- src/backend/main/index.js
+++ src/backend/main/index.js
@@ -2,6 +2,7 @@ import DeviceList from './device-list.js'
 import DeviceController from '../controller/device-controller.js'
 import StorageManager from '../service/storage-manager.js'
 import AddDeviceModal from './add-device-modal.js'
+import UpdateDeviceModal from './update-device-modal.js'
 import { buttonOpenModal, buttonCloseModal, buttonAddDevice } from '../events/events-factory.js'
 class MainApplication {
   constructor (storageManager) {
@@ -13,18 +14,36 @@ class MainApplication {
     const modalAddDevice = document.getElementById(ids.modalAddDevice)
     const inputNewDeviceTag = document.getElementById(ids.newTagDeviceInput)
 
-    const deviceController = new DeviceController(this.storageManager)
-    const deviceList = new DeviceList(deviceListEl, deviceController)
+    const modalUpdateDevice = document.getElementById(ids.modalUpdateDevice)
+    const modalTagDevice = document.getElementById(ids.modalTagDevice)
+    const modalStateDevice = document.getElementById(ids.modalStateDevice)
+    const modalAttributesListDevice = document.getElementById(ids.modalAttributesListDevice)
+    const modalBtUpdateDevice = document.getElementById(ids.modalBtUpdateDevice)
+    const modalBtCancelDevice = document.getElementById(ids.modalBtCancelDevice)
+    const modalBtDeleteDevice = document.getElementById(ids.modalBtDeleteDevice)
+
     const modalElement = new AddDeviceModal(modalAddDevice)
 
+    const updateModalEl = new UpdateDeviceModal(
+      modalUpdateDevice, modalTagDevice, modalStateDevice,
+      modalAttributesListDevice, modalBtUpdateDevice, modalBtCancelDevice,
+      modalBtDeleteDevice
+    )
+    const deviceController = new DeviceController(this.storageManager)
+    const deviceList = new DeviceList(deviceListEl, deviceController, updateModalEl)
     await deviceList.initialize()
     buttonOpenModal(ids.btOpenAddModal, modalElement)
     buttonCloseModal(ids.btCancelAddDevice, modalElement)
     buttonAddDevice(ids.btConfirmAddDevice, deviceList, inputNewDeviceTag)
 
+    buttonCloseModal(ids.modalBtCancelDevice, updateModalEl)
+
     window.onclick = (event) => {
       if (event.target === modalAddDevice) {
         modalElement.closeModal()
+      }
+      if (event.target === modalUpdateDevice) {
+        updateModalEl.closeModal()
       }
     }
   }
@@ -40,7 +59,14 @@ window.onload = async function () {
     btCancelAddDevice: 'bt-cancel-add-device',
     devicesList: 'devices-list',
     modalAddDevice: 'modal-add-device',
-    newTagDeviceInput: 'new-device-tag'
+    newTagDeviceInput: 'new-device-tag',
+    modalUpdateDevice: 'modal-device',
+    modalTagDevice: 'modal-device-tag',
+    modalStateDevice: 'modal-device-state',
+    modalAttributesListDevice: 'modal-device-attributes',
+    modalBtUpdateDevice: 'bt-modal-confirm-update-device',
+    modalBtCancelDevice: 'bt-modal-cancel-update-device',
+    modalBtDeleteDevice: 'bt-modal-delete-device'
   })
 }
 require('electron').remote.getCurrentWindow().on('close', async (e) => {
