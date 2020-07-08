88267dd6004d3c87f03193f6045faabcda2ef524
Luis Carlos Galvão <dev01@mowahtecnologia.com>
2020-07-08 10:44:07 -0400

    feat: create listeners correctly

---
+++ src/backend/main.js
@@ -0,0 +1,24 @@
+const { ipcMain, app } = require('electron')
+const StorageManager = require('./service/storage-manager')
+const DeviceController = require('./controller/device-controller')
+module.exports = class MainApplication {
+  async run () {
+    await StorageManager.load()
+    const deviceController = new DeviceController(StorageManager)
+    ipcMain.on('device:save', async (event, deviceTag) => {
+      const newDevice = await deviceController.add(deviceTag)
+      if (newDevice) {
+        console.log('salvou')
+      }
+      event.returnValue = newDevice
+    })
+    ipcMain.on('device:load', (event, arg) => {
+      const devices = deviceController.getDevices()
+      event.returnValue = devices
+    })
+
+    app.on('window-all-closed', async () => {
+      await StorageManager.save()
+    })
+  }
+}
