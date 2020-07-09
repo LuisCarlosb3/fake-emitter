27727d0f173f87d61b0a16ef860655be38e61367
Luis Carlos Galvão <dev01@mowahtecnologia.com>
2020-07-09 15:55:09 -0400

    feat: initialize mqtt modal

--- src/frontend/js/main.js
+++ src/frontend/js/main.js
@@ -1,6 +1,7 @@
 import { openAddDeviceModal, closeAddDeviceModal, submitAddDeviceModal } from '../js/functions-add-device-events.js'
 import { initializeList, handleListChanges } from './functions-list-devices-events.js'
 import { openDeviceModal, initializeDeviceModalEvents } from './functions-device-modal-events.js'
+import { openMqttModalEvent, buttonCloseModal } from './functions-mqtt-modal-events.js'
 function initializeElements () {
   // Add device modal
   openAddDeviceModal()
@@ -11,5 +12,8 @@ function initializeElements () {
   handleListChanges(openDeviceModal)
   // Update device modal
   initializeDeviceModalEvents()
+  // mqtt
+  openMqttModalEvent()
+  buttonCloseModal()
 }
 window.onload = initializeElements
