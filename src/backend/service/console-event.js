fe4d0dc89d0657e0738c6b1bd009f8d06524ce4d
Luis Carlos Galvão <dev01@mowahtecnologia.com>
2020-07-09 17:29:37 -0400

    feat: remove unused logs

--- src/backend/service/console-event.js
+++ src/backend/service/console-event.js
@@ -1,7 +1,6 @@
 const { webContents } = require('electron')
 module.exports = function showConsole (data) {
   const webCont = webContents.fromId(2)
-  console.log(data)
   if (webCont) {
     webCont.send('console:log', data)
   }
